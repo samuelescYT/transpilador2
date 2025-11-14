9b6645a875d1cd2b49ffa91a505dbf2d2555352d
samuelescYT <89674580+samuelescYT@users.noreply.github.com>
2025-11-14 15:06:31 -0500

    Update app to use environment variable for port

--- backend/app.py
+++ backend/app.py
@@ -1525,4 +1525,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
